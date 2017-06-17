fb5c98826cda96469fbaaec9c58896376edf8a3c
Florencia Sepulveda <florenciasepulveda.26@gmail.com>
2017-06-17 18:09:35 -0300

    login

--- acciones/login.php
+++ acciones/login.php
@@ -2,7 +2,20 @@
 	
 	//include( '../../setup/config.php' ); 
 	
-	
+	//borrar esto cuando este la conexion desde el config
+		$dbini = parse_ini_file('../setup/online.ini');
+
+		//conexion al sql
+		$conexion = @mysqli_connect(
+			$dbini['host'],
+			$dbini['user'],
+			$dbini['pwd'],
+			$dbini['bdd']
+		);
+
+		if( $conexion ){
+			mysqli_set_charset($conexion, 'utf8');
+		}
 
 	/* CAMPOS LOGIN FORM */
 	$nombre_login = $_POST['usuario'];
@@ -14,9 +27,7 @@ SELECT
 	IDUSUARIOS, 
 	NOMBRE_USUARIO, 
 	CONTRASENIA, 
-	U_ESTADO, 
-	FKPERMISOS, 
-	ESTADO, 
+	U_ESTADO
 FROM usuarios 
 WHERE 
 	NOMBRE_USUARIO='$nombre_login' AND CONTRASENIA='$clave_login' 
@@ -26,17 +37,18 @@ LOGIN;
 	$user = mysqli_query($conexion, $c_log);
 	$log = mysqli_fetch_assoc($user);
 
-	if( !$log ){
+	if( ! $log ){
 		$_SESSION['LOGIN_ERROR'] = 'Mal usuario o clave';
+		var_dump($_SESSION['LOGIN_ERROR']);
 	} else {
 		if( $log['U_ESTADO'] == 1 ){
 			$_SESSION = $log;
+			var_dump($log);
 		} else {
 			$_SESSION['LOGIN_ERROR'] = 'Cuenta bloqueada';
+			var_dump($_SESSION['LOGIN_ERROR']);
 		}
 	}
 
 	//header("Location: ../index.php");
-
-	var_dump($log);
 ?>
