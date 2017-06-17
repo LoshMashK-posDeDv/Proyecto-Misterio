733e9f7f880534726afa31d4f00243819416da67
Florencia Sepulveda <florenciasepulveda.26@gmail.com>
2017-06-17 20:01:20 -0300

    registro

--- acciones/registro.php
+++ acciones/registro.php
@@ -1,27 +1,27 @@
 <?php 
-	include( '../setup/config.php' );
+include( '../setup/config.php' );
 
-	$nombre_completo = $_POST['nombre_completo'];
-	$email = $_POST['email'];
-	$nombre_usuario = $_POST['usuario'];
-	$password = md5($_POST['password']);
+$nombre_completo = $_POST['nombre_completo'];
+$email = $_POST['email'];
+$nombre_usuario = $_POST['usuario'];
+$password = md5($_POST['password']);
 
 $c_reg = <<<REGISTRO
-	INSERT INTO
-		usuarios
-	SET 
-		NOMBRE_COMPLETO = '$nombre_completo',
-		NOMBRE_USUARIO = '$nombre_usuario',
-		EMAIL = '$email',
-		CONTRASENIA = '$password',
-		U_ESTADO = '1',
-		FKPERMISO = 'user',
-		FECHA_ALTA = NOW()
+INSERT INTO
+	usuarios
+SET 
+	NOMBRE_USUARIO = '$nombre_usuario',
+	CONTRASENIA = '$password',
+	U_ESTADO = '1',
+	FKPERMISO = 'user',
+	NOMBRE_COMPLETO = '$nombre_completo',
+	EMAIL = '$email',
+	FECHA_ALTA = NOW( )
 REGISTRO;
 
 	mysqli_query($conexion, $c_reg);
 	//header("Location: registro_Form.php");
 
-	var_dump($c_reg );
+	var_dump(mysqli_query($conexion, $c_reg));
 
 ?>
