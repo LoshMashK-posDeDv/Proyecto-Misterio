0dea1a5307bdd18611f290193518af4ae629c73f
Leo Merlo <lmerlo@perceptyx.com>
2017-06-17 18:22:41 -0300

    Form basico para agregar video

---
+++ admin/modulos/video_agregar.php
@@ -0,0 +1,40 @@
+<div class="content-fluid">
+	<form class="create_form" action="../acciones/agregar_video.php" eenctype="multipart/form-data" method="post">
+		<div class="section__title">
+			<h2><input type="text" name="titulo" id="titulo" placeholder="Titulo del video" class="create_form__title"/></h2>
+		</div>
+
+		<div>
+			<h3>Descripcion</h3>
+			<textarea class="create_form__description" name="descripcion" id="descripcion"></textarea>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<h3>Duraci&oacute;n</h3>
+				<input type="text" name="duracion" id="duracion" />
+			</div>
+			<div class="col-md-6">
+				<h3>A&nacute;o</h3>
+				<input type="text" name="anio" id="anio" />
+			</div>
+		</div>
+
+		<div>
+			<h3>Video</h3>
+			<input type="file" name="video" id="video" />
+		</div>
+
+		<div>
+			<h3>Imagenes</h3>
+			<input type="file" name="imagenes" id="imagenes" />
+		</div>
+
+		<div>
+			<h3>Imagen destacada</h3>
+			<input type="file" name="imagen_destacada" id="imagen_destacada" />
+		</div>
+
+		<input type="submit" class="btn" />
+	</form>
+</div>
--- admin/modulos/videos_listado.php
+++ admin/modulos/videos_listado.php
@@ -8,8 +8,8 @@
 		<tr class="video_list__head">
 			<th class="video_list__head__image">Captura</th>
 			<th class="video_list__head__name">Título</th>
-			<th class="hidden-xs">Autor</th>
-			<th class="hidden-xs">Fecha</th>
+			<th class="video_list__head__author hidden-xs">Autor</th>
+			<th class="video_list__head__date hidden-xs">Fecha</th>
 			<th class="video_list__head__actions">Acción</th>
 		</tr>
 	</thead>
@@ -18,8 +18,8 @@
 		<tr class="video_list__row">
 			<td class="video_list__row__image"><img src="http://placehold.it/100x80"></td>
 			<td class="video_list__row__name"><p>Lorem ipsum dolor sit amet.</p></td>
-			<td class="hidden-xs"><p>Admin</p></td>
-			<td class="hidden-xs"><p>21/01/2017</p></td>
+			<td class="video_list__row__author hidden-xs"><p>Admin</p></td>
+			<td class="video_list__row__date hidden-xs"><p>21/01/2017</p></td>
 			<td class="video_list__row__actions">
 				<a href="#" title="Editar video"><i class="glyphicon glyphicon-pencil"></i></a>
 				<a href="#" title="Eliminar video"><i class="glyphicon glyphicon-remove"></i></a>
