8de2e9fe217e76031dea7594bfb2ca4d71db2c28
Laura <mlauramitono@gmail.com>
2017-06-17 18:16:21 -0300

    Agregado el include del config en el index + generados los archivos ini con la conexion + agregue cositas en el config.php

--- setup/config.php
+++ setup/config.php
@@ -1,5 +1,28 @@
 <?php
 
+//leo el archivo de configuración general
+$config_gen = parse_ini_file('setup.ini',true);
+
+date_default_timezone_set($config_gen['zona']['timezone']);
+
+//conexion local u online
+if($_SERVER['HTTP_HOST'] == 'localhost'){
+	$config = parse_ini_file('local.ini');
+}else{
+	$config = parse_ini_file('online.ini');
+}
+error_reporting($config['errores']['reporting']);
+ini_set('display_errors' , $config['errores']['display']) ;
+
+//conexion al sql
+$conexion = @mysqli_connect(
+	$config['host'],
+	$config['user'],
+	$config['pwd'],
+	$config['bdd']
+);
+
+
 // To do:
 // - Validacion del get
 // - Sanitizacion del get
