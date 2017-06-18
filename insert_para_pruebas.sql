467deed5a9c8b14a9f96487ac4b09326e7aebe31
Laura <mlauramitono@gmail.com>
2017-06-17 23:47:15 -0300

    Mini insert que funciona en el query pero a mi no se me actualiza la tabla

---
+++ insert_para_pruebas.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+	articulos (TITULO, AUTOR, AÑO, DURACION, DESCRIPCION)
+VALUES
+	('La pandilla', 'Quiensabe', 1985, 00:02:55, 'Saraza'),
+	('El tp dificil', 'Ger', 2015, 00:05:50, 'Saraza'),
+	('La moneda', 'Sari', 2400, 00:06:05, 'Saraza'),
+	('El tostadito de Dv', 'Señora del buffet', 1954, 00:03:30, 'Saraza'),
+	('Fantasmitas', 'Joni', 2017, 00:02:55, 'Saraza'),
+	('Muchas cositas', 'Flor', 2014, 00:05:50, 'Saraza'),
+	('Que puede ser', 'Alguien', 2018, 00:03:30, 'Saraza');
