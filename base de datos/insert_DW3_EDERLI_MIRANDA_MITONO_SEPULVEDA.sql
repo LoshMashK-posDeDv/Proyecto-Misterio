4adb47a39ecaee367c80704cce20cba8a14910fa
Laura <mlauramitono@gmail.com>
2017-08-06 19:11:50 -0300

    corregidos errores de sintaxis

--- base de datos/insert_DW3_EDERLI_MIRANDA_MITONO_SEPULVEDA.sql
+++ base de datos/insert_DW3_EDERLI_MIRANDA_MITONO_SEPULVEDA.sql
@@ -24,7 +24,7 @@ INSERT INTO
 VALUES
 	('Miren mi ropita nueva de Splatoon', 'Splatoon', NOW( ), null, null, 'splatoon.jpg', 'Hola amigos, estuve jugando al Splatton 2 para Nintendo Swith y me sorprendió la variedad de ropitas nuevas que hay disponible.', '1', 5),
 	('Through The Ages 2nd Edition!', 'Through The Ages', NOW( ), null, 'through02.jpg, through03.jpg, through04.jpg', 'through01.jpg', 'Con Sid Meier, kpo total de la estrategia en la pc hace años y años!! Gracias por tanto, pequeño homenaje ingame a el, el lider mas codiciado!', '1', 1),
-	('Ticket to Ride: Europe', 'Ticket to Ride', NOW( ), null, 'ticket.jpeg', 'Uno de mis jueguitos favoritos, super adictivo, puedo pasarme horas jugando con mis amigas Flor', '1', 6),
+	('Ticket to Ride: Europe', 'Ticket to Ride', NOW( ), null, null, 'ticket.jpeg', 'Uno de mis jueguitos favoritos, super adictivo, puedo pasarme horas jugando con mis amigas Flor', '1', 6),
 	('Deadpool Funko Pop', 'Deadpool', NOW( ), null, null, 'deadpool.jpg', 'Miren el muñequito que me compré', '1', 2),
 	('Mi stitch y yo', 'stitch', NOW( ), null, null, 'stitch.jpg', 'Stitch rechazándome fuertemente un domingo por la noche', '1', 5),
 	('Listo para matar', 'Deadpool', NOW( ), null, null, 'deadpoolger.jpg', 'A punto de hacerles bebes a mis alumnos de php con mi nueva campera de Deadpool', '1', 1);
@@ -32,28 +32,28 @@ VALUES
 INSERT INTO
 	comentarios (COMENTARIO, FECHA_COMENTARIO, C_ESTADO, FKUSUARIO, FKARTICULO)
 VALUES
-	('Este video está genial', NOW( ), '1', 1, 2),
-	('Quiero que vuelvan a subir más videos como este', NOW( ), '1', 6, 3),
-	('Me parece asombroso', NOW( ), '1', 5, 8),
-	('Es una porquería', NOW( ), '0', 9, 4),
-	('Que copado que está, voy a volver a verlo', NOW( ), '1', 2, 14),
-	('Se mandaron cualquiera', NOW( ), '0', 5, 6),
-	('Simplemente maravilloso', NOW( ), '1', 3, 12),
-	('Tiene una estetica asombrosa', NOW( ), '1', 4, 5);
+	('Esa ropita está genial', NOW( ), '1', 4, 1),
+	('Es un juego fantástico', NOW( ), '1', 6, 2),
+	('Me parece asombroso', NOW( ), '1', 5, 3),
+	('Es una porquería', NOW( ), '0', 8, 4),
+	('que rica piba', NOW( ), '1', 5, 5),
+	('De lo mejor que se vio en este último tiempo', NOW( ), '0', 4, 1),
+	('Altas llantas', NOW( ), '1', 7, 1),
+	('Está muy bueno pero son 8 horas de juego', NOW( ), '1', 5, 2);
 
 INSERT INTO
 	categorias (CATEGORIA)
 VALUES
-	('Gameplay'), --1
-	('Como jugar'), --2
-	('Unboxing'), --3
-	('Opinion/Review'), --4
-	('Fan Made'), --5
-	('Homenaje'), --6
-	('Animacion'), --7
-	('Live action'), --8
-	('Random / Misc'), --9
-	('Bragging'); --10
+	('Gameplay'), 
+	('Como jugar'), 
+	('Unboxing'), 
+	('Opinion/Review'), 
+	('Fan Made'), 
+	('Homenaje'), 
+	('Animacion'), 
+	('Live action'), 
+	('Random / Misc'),
+	('Bragging'); 
 
 INSERT INTO
 	articulos_categorias
