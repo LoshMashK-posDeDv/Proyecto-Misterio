2ca02b6b7e1dcf6b33e7ab3da2a252cfb2b7883a
Florencia Sepulveda <florenciasepulveda.26@gmail.com>
2017-08-08 13:12:40 -0300

    estilitos

--- modulos/recupero_pass.php
+++ modulos/recupero_pass.php
@@ -2,4 +2,14 @@
 	<h2>Recuperar contraseña</h2>
 	<div class="login_form__row"><input type="text" name="usuario" placeholder="Email"></div>
 	<div class="login_form__row"><input type="submit" name="enviar" value="Enviar" class="btn btn_ok btn-lg btn-block"></div>
+
+	<?php 
+		if (isset($_GET['m'])) {
+			if ($_GET['m'] == 'error') {
+				echo '<p class="error">';
+					echo 'El usuario no ha sido registrado.';
+				echo '</p>';
+			}
+		}		
+	?>
 </form> 
