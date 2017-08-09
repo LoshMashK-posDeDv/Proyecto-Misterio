8dfdcbc92c4ec035963b2838fe7da4a89adc9622
Florencia Sepulveda <florenciasepulveda.26@gmail.com>
2017-08-09 08:56:28 -0300

    about

--- modulos/about.php
+++ modulos/about.php
@@ -1,63 +1,75 @@
-<div class="container about">
-	<h2 class="u text-left">Hola, simple mortal!</h2>
-	<p>Este sitio no fue sólo desarrollado para la diversión de sus creadores, si no que es parte de una entrega. Acá vas a poder encontrar la información de la misma y de como usar este maravilloso sitio.</p>
+<div class="container ">
+	<div class="about">
+		<h2 class="u text-left">Hola, simple mortal!</h2>
+		<p>Este sitio no fue sólo desarrollado para la diversión de sus creadores, si no que es parte de una entrega. Acá vas a poder encontrar la información de la misma y de como usar este maravilloso sitio.</p>
+		
+		<div class="flex">
+			<div class="about-box box-first">
+				<h3>Info Entrega</h3>
+				<ul>
+					<li>Materia: Programación II</li>
+					<li>Examen Final</li>
+					<li>Profesor: Germán Rodríguez</li>
+					<li>DW3ATN - 2017</li>
+					<li>Alumnos:</li>
+					<ul>
+						<li>Ederli, Florencia</li>
+						<li>Miranda, Federico</li>
+						<li>Mitono, Laura</li>
+						<li>Sepúlveda, Florencia</li>
+					</ul>
+				</ul>
+			</div>
 
-	<h3>Info Entrega</h3>
-		<ul>
-			<li>Materia: Programación II</li>
-			<li>Examen Final</li>
-			<li>Profesor: Germán Rodríguez</li>
-			<li>DW3ATN - 2017</li>
-			<li>Alumnos:</li>
-			<ul>
-				<li>Ederli, Florencia</li>
-				<li>Miranda, Federico</li>
-				<li>Mitono, Laura</li>
-				<li>Sepúlveda, Florencia</li>
-			</ul>
-		</ul>
-
-	<h3>Info sitio</h3>
-
-	<p>La función principal de este sitio es la de darle al usuario un espacio para postear fotos, videos y textos de diferentes cuestiones relacionadas a series, películas, juegos y demases nerdeadas.</p>
-
-	<h4>Header</h4>
-	<p>En el header vamos a poder navegar a la página principal del sitio, ver todos los posteos, obtener información del sitio, acceder al panel de usuario y buuuuscar (entienden? Buuuuuuscar! Es un fantasmita!).</p>
-	<p>De acuerdo a si el usuario está logueado o no al sitio, va a ver diferentes opciones en el header.</p>
-	<p>Por default va a ver la opción para loguearse o registrarse:</p>
-	<img src="images/about/login.png" alt="header con opción para loguearse o registrarse">
-	<p>Una vez introducido el usuario y contraseña, el usuario va a ver la opción para acceder al panel y la opción para desloguearse:</p>
-	<img src="images/about/cuenta.png" alt="header con opciones para el usuario logueado">
-	<p>Usuario y psw de administrador:</p>
-	<ul>
-		<li>german.rodriguez@davinci.edu.ar</li>
-		<li>admin1234</li>
-	</ul>
-
-	<h4>Panel</h4>
-	<p>Una vez dentro del panel, dependiendo el rol (Administrador/User), el usuario va a poder ver distintas opciones:</p>
-	<h5>User</h5>
-	<p>El usuario común va a poder ver las siguientes opciones:</p>
-	<img src="images/about/usernormal.png" alt="panel como lo ve el usuario normal">
-	<ul>
-		<li>Perfil: Acá va a poder ver y modificar sus datos personales.</li>
-		<li>Chucherías: Va a ver sólo sus posteos y va a poder editarlos o borrarlos.</li>
-		<li>Cerrar Sesión</li>
-	</ul>
-	<h5>Admin</h5>
-	<p>El usuario administrador en cambio va a poder ver las siguientes opciones:</p>
-	<img src="images/about/panel.png" alt="panel como lo ve el usuario normal">
-	<ul>
-		<li>Panel: Acá se puede ver el informe de actividad del sitio, así como exportarlo en PDF.</li>
-		<li>Perfil: Acá va a poder ver y modificar sus datos personales.</li>
-		<li>Usuarios: Lista completa de usuarios dónde se puede cambiar su tipo, estado o borrar.</li>
-		<li>Comentarios: Lista de comentarios, con fecha y posibilidad de borrarlo.</li>
-		<li>Chucherías: Va a ver todos los posteos y va a poder editarlos o borrarlos.</li>
-		<li>Cerrar Sesión</li>
-	</ul>
-
-	<h4>Footer</h4>
-	<p>Ya viste la magia del footer?</p>
-
-
+			<div class="about-box box-second">
+				<h3>Info sitio</h3>
+				<p>La función principal de este sitio es la de darle al usuario un espacio para postear fotos, videos y textos de diferentes cuestiones relacionadas a series, películas, juegos y demases nerdeadas.</p>
+				
+				<div class="border_box">
+					<h4>Header</h4>
+					<p>En el header vamos a poder navegar a la página principal del sitio, ver todos los posteos, obtener información del sitio, acceder al panel de usuario y buuuuscar (entienden? Buuuuuuscar! Es un fantasmita!).</p>
+					<p>De acuerdo a si el usuario está logueado o no al sitio, va a ver diferentes opciones en el header.</p>
+					<p>Por default va a ver la opción para loguearse o registrarse:</p>
+					<img src="images/about/login.png" alt="header con opción para loguearse o registrarse">
+					<p>Una vez introducido el usuario y contraseña, el usuario va a ver la opción para acceder al panel y la opción para desloguearse:</p>
+					<img src="images/about/cuenta.png" alt="header con opciones para el usuario logueado">
+					<p>Usuario y psw de administrador:</p>
+					<ul>
+						<li class="green">german.rodriguez@davinci.edu.ar</li>
+						<li>admin1234</li>
+					</ul>				
+				</div>
+				
+				<div class="border_box">
+					<h4>Panel</h4>
+					<p>Una vez dentro del panel, dependiendo el rol (Administrador/User), el usuario va a poder ver distintas opciones:</p>
+					<h5>User</h5>
+					<p>El usuario común va a poder ver las siguientes opciones:</p>
+					<img src="images/about/usernormal.png" alt="panel como lo ve el usuario normal">
+					<ul>
+						<li><span class="bold">Perfil:</span> Acá va a poder ver y modificar sus datos personales.</li>
+						<li><span class="bold">Chucherías:</span> Va a ver sólo sus posteos y va a poder editarlos o borrarlos.</li>
+						<li>Cerrar Sesión</li>
+					</ul>
+					<h5>Admin</h5>
+					<p>El usuario administrador en cambio va a poder ver las siguientes opciones:</p>
+					<img src="images/about/panel.png" alt="panel como lo ve el usuario normal">
+					<ul>
+						<li><span class="bold">Panel:</span> Acá se puede ver el informe de actividad del sitio, así como exportarlo en PDF.</li>
+						<li><span class="bold">Perfil:</span> Acá va a poder ver y modificar sus datos personales.</li>
+						<li><span class="bold">Usuarios:</span> Lista completa de usuarios dónde se puede cambiar su tipo, estado o borrar.</li>
+						<li><span class="bold">Comentarios:</span> Lista de comentarios, con fecha y posibilidad de borrarlo.</li>
+						<li><span class="bold">Chucherías:</span> Va a ver todos los posteos y va a poder editarlos o borrarlos.</li>
+						<li>Cerrar Sesión</li>
+					</ul>
+				</div>
+					
+				<div class="border_box">
+					<h4>Footer</h4>
+					<p>Ya viste la magia del footer?</p>
+				</div>
+			</div>
+		</div>
+			
+	</div>
 </div>
