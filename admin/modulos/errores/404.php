79596d28498077f08538f3ca7460cf195ed5d650
fespinoX <florencia.espino@gmail.com>
2017-08-09 15:42:30 -0300

    Errores

---
+++ admin/modulos/errores/404.php
@@ -0,0 +1,15 @@
+
+
+<section class="section--home--proyecto">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-sm-12">
+
+				<img src="../images/errores/error404.jpg" alt="error 404" style="max-width: 100%;" />
+				<!-- <h2 class="sr-only">404</h2>
+				<p class="sr-only">No se encontró la página que estás buscando. Seguramente la dejaste en el mismo lugar dónde perdiste tu dignidad.</p> -->
+			</div>
+		</div>		
+	</div>
+</section>
