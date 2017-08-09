8c14832dad4ff2f047adfc6b0be6eba53a3f8c48
Florencia Sepulveda <florenciasepulveda.26@gmail.com>
2017-08-09 00:54:58 -0300

    -

--- admin/acciones/editar_perfil.php
+++ admin/acciones/editar_perfil.php
@@ -6,33 +6,34 @@
     $nick = $_POST['nick'];
     $password = $_POST['password'];
     $edad = $_POST['edad'];
-    //$password_confirm = $_POST['password_confirm'];
+    $password_confirm = $_POST['password_confirm'];
 
     $er_nombre = "/^[\w\s]{3,50}$/i";   
-    $er_email = "/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/";  
-    $er_nick = "/^[\d\w\s]{5,50}$/";
+    $er_email = "/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i";  
+    $er_nick = "/^[\d\w\s]{3,50}$/";
     $er_edad = "/^[0-9]{1,3}$/";
     $er_pass = "/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])\S{8,15}$/";
 
-    $val_nombre = preg_match($er_nombre, $nombre, $coincidencia_nombre);
-    $val_email = preg_match($er_email, $email, $coincidencia_email);
-    $val_edad = preg_match($er_edad, $edad, $coincidencia_edad);
-    $val_nick = preg_match($er_nick, $nick, $coincidencia_nick);
-    $val_pass = preg_match($er_pass, $password, $coincidencia_pass);    
+    $val_nombre = preg_match($er_nombre, $nombre);
+    $val_email = preg_match($er_email, $email);
+    $val_edad = preg_match($er_edad, $edad);
+    $val_nick = preg_match($er_nick, $nick);
+    $val_pass = preg_match($er_pass, $password);    
     
-    /* Como el campo de la contraseña no es obligatorio, primero quiero saber si me llegó algo.
-    Si llegó algo, lo tengo que pasar por el md5 para la BD.
-    Después, adentro del if que hace el UPDATE mi idea era comparar $pass con $pass_confirm y solo en caso de que fueran iguales (si existen), hacer el update.*/
-
-    /* if (!empty($password)) {
-        var_dump($password);
-    }*/ 
-
     if($val_nombre && $val_email && $val_nick){
-        
-        $actualizar_datos = "UPDATE usuarios SET NOMBRE_COMPLETO = '$nombre', EMAIL = '$email', NICK = '$nick' WHERE EMAIL = '$_SESSION[IDUSUARIOS]' LIMIT 1";
+        if (!empty($password) && !empty($password_confirm)) {
+            if ($password == $password_confirm) {
+                $password = md5($password);
+                $actualizar_datos = "UPDATE usuarios SET NOMBRE_COMPLETO = '$nombre', CONTRASENIA = '$password', EMAIL = '$email', NICK = '$nick' WHERE EMAIL = '$_SESSION[IDUSUARIOS]' LIMIT 1";
+                var_dump('Actualizar con contraseña nueva');
+            }
+        } else {
+            $actualizar_datos = "UPDATE usuarios SET NOMBRE_COMPLETO = '$nombre', EMAIL = '$email', NICK = '$nick' WHERE EMAIL = '$_SESSION[IDUSUARIOS]' LIMIT 1";
+                var_dump('Actualizar SIN contraseña nueva');
+        }
 
         mysqli_query($conexion, $actualizar_datos);
+        var_dump($actualizar_datos);
 
         $rta = 'ok';
         //header("Location: ../index.php?s=mi_cuenta&rta=$rta");
